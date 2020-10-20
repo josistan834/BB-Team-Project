51cb4b2c8522d1e3aa2ff5cd31ad2e8ac10dd22a
Josiah Stanley <josistan834@ed.amdsb.ca>
2020-10-20 11:36:15 -0400

    fixed paddle

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -111,9 +111,12 @@ namespace BrickBreaker
             {
                 lives--;
 
+                //Move paddle to middle
+                paddle.x = (this.Width / 2 - paddle.width);
                 // Moves the ball back to origin
                 ball.x = ((paddle.x - (ball.size / 2)) + (paddle.width / 2));
                 ball.y = (this.Height - paddle.height) - 85;
+                
 
                 if (lives == 0)
                 {
